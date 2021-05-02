68b5f5c9d11cf784857979d00c6e3aae2f46e6a3
DESKTOP-40OA392\User <awp10520@gmail.com>
2021-05-02 22:28:57 +0800

    redux counter pratice finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment,incrementByAmount } from './counter';
 
-function App() {
+const App = () => {
+  const { count } = useSelector( (state) => state.counter);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>The Count is {count}</h1>
+      <button onClick = {()=>{dispatch(increment())}}>
+          increment
+      </button>
+      <button onClick = {()=>{dispatch(decrement())}}>
+          decrement
+      </button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        Increment by 33
+      </button>
+    </>
   );
 }
 
